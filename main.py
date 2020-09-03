0d52f5af4c2382f45b77a14a4c0006de2cc061f4
Scharnick <F2875616@fnb.co.za>
2020-09-03 17:39:52 +0200

    Hello FastApi

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Item(BaseModel):
     is_offer: Optional[bool] = None
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "FastApi"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
