08585b277aa40ff66ddc1f6f8ab1d658cb114ef0
Scharnick <F2875616@fnb.co.za>
2020-09-04 19:28:44 +0200

    Added Any

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
+
+class Alert(BaseModel):
+    name: str
+    description: Optional[str] = None
+    dateTime: int = None
+    tax: Optional[float] = None
+
+class City(BaseModel):
+    name: str
+    timezone: str 
+ 
+class Any(BaseModel):
+    any: dict
+
+db = []
+
 @app.get("/")
 def read_root():
     return {"Hello": "FastApi"}
@@ -16,6 +32,27 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get('/cities')
+def get_cities():
+    return db
+
+# @app.get('/cities/{city_id}')
+
+@app.post('/cities')
+def create_city(city: City):
+    db.append(city.dict())
+    return db[-1] 
+
+@app.post('/any')
+def create_any(mydict: dict):
+    return mydict
+
+# @app.delete('/cities')
+
 @app.put("/items/{item_id}")
 def udpate_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/meraki/alerts/")
+def meraki(q: Optional[str]):
+    return {q: q}
