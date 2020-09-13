34323d97d905f77cc5c7da418541c654908623c3
retlaw <scharnick@gmail.com>
2020-09-13 14:03:51 +0200

    go-back

--- models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, Integer
-from database import Base
-
-class Posts(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    post = Column(String)
